80326ba8d74be39cb370345c53923c5790cbe9a0
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-05-23 19:44:38 -0300

    store counters in db

--- app/main.js
+++ app/main.js
@@ -6,31 +6,44 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  db.get(
+    `update counters
+      set value = value + 1
+      where name = 'accesses'
+      returning value`,
+    (err, row) => {
+      if (err !== null) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`Error while updating counter: ${JSON.stringify(err)}`);
+      }
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(`This page has been accessed ${row.value} time${row.value == 1 ? "" : "s"}`);
+    }
+  );
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+
+
 // database base code
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
+/* split table creation/initialization and run inside serialize to prevent
+  insertions from encountering a missing table */
 db.serialize(() => {
-  db.run("CREATE TABLE lorem (info TEXT)");
-
-  const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (let i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-      console.log(row.id + ": " + row.info);
-  });
+  db.run(`create table counters (
+    name primary key,
+    value
+  );`)
+  .run(`insert into counters (name, value)
+    values ("accesses", 0);`
+  );
 });
-
-db.close();
