c6f7da73b99d9e5e830200beffcb149e17f689d0
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-05-29 11:13:28 -0300

    /appointment: html dummy

--- app/request-handlers/appointment.js
+++ app/request-handlers/appointment.js
@@ -14,9 +14,29 @@ function formBody(req) {
 
 async function respond(req, res, db) {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
   const body = await formBody(req);
-  res.end(`User ${ body.userid }, you have your appointment.`);
+  res.end(`\
+<!DOCTYPE html>
+<html lang="en" class="booting">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <title>Your appointment is ready</title>
+
+  <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+  <!-- <link rel="icon" href="favicon.ico" type="image/x-icon" /> -->
+  <!-- <meta name="description" content="blurb for google search" />  -->
+  <!-- <link rel="canonical" href="www.mysite.com/index.html" > -->
+
+  <!-- <link rel="stylesheet" href="my-css-file.css" /> -->
+  <!-- <script src="main.js" module></script> -->
+</head>
+<body>
+  <p>User ${ body.userid }, you have your appointment.<p/>
+</body>
+</html>`
+  );
 }
 
 export default { match, respond };
