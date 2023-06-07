f87248495c32f3bcf0cadc2497ff78594eab6438
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-06-07 11:24:15 -0300

    updated formatting

--- app/request-handlers/database-wrapper.js
+++ app/request-handlers/database-wrapper.js
@@ -3,61 +3,58 @@ import sqlite3 from 'sqlite3';
 // TODO: 'userobj' is the same as the form in index.html
 
 function WrapperDatabase() {
-    this.db = new sqlite3.Database(':memory:');
+  this.db = new sqlite3.Database(':memory:');
 
-    /* split table creation/initialization and run inside serialize to prevent
-    insertions from encountering a missing table */
+  /* split table creation/initialization and run inside serialize to prevent
+  insertions from encountering a missing table */
+  this.db.serialize(() => {
+    this.db.run(`create table counters (
+      name primary key,
+      value
+    );`)
+    .run(`insert into counters (name, value)
+      values ("accesses", 0);`)
+
+
+    // create table 'users'
+
+    .run(`create table users (user_id primary key, email);`);
+  });
+
+  this.addUser = function(userobj) {
     this.db.serialize(() => {
-        this.db.run(`create table counters (
-        name primary key,
-        value
-        );`)
-        .run(`insert into counters (name, value)
-        values ("accesses", 0);`)
-
-
-        // create table 'users'
-
-        .run(`create table users (
-        user_id primary key,
-        email
-        );`);
+      this.db.run(`insert into users (user_id, email)
+        values (${userobj.userid}, ${userobj.email});`
+      );
     });
+  },
 
-    this.addUser = function(userobj) {
-        this.db.serialize(() => {
-            this.db.run(`insert into users (user_id, email)
-            values (${userobj.userid}, ${userobj.email});`
-            );
-        });
-    },
+  // if first digit is 1, has appointment
+  this.hasUser = function(userobj) {
+    return this.db.serialize(() => {
+      this.db.get(
+        `select (?, ?) from users;`,
+        [ userobj.userid, userobj.email ],
+        (err, row) => row != undefined
+      );
+    })
+  },
 
-    // if first digit is 1, has appointment
-    this.hasUser = function(userobj) {
-        return this.db.serialize(() => {
-            this.db.get(
-                `select (?, ?) from users;`,
-                [ userobj.userid, userobj.email ],
-                (err, row) => row != undefined
-            );
-        })
-    },
+  // if even, it has an appointment
+  this.hasAppointment = function(userobj) {
+    return this.hasUser(userobj) && (userobj.userid % 2 == 0);
+  },
 
-    // if even, it has an appointment
-    this.hasAppointment = function(userobj) {
-        return this.hasUser(userobj) && (userobj.userid % 2 == 0);
-    },
+  this.fetchAppointment = function(userobj) {
+    return "saturday the 14th";
+  },
 
-    this.fetchAppointment = function(userobj) {
-        return "saturday the 14th";
-    },
-
-    this.createAppointment = function(userobj) {
-        return "sunday the 15th"
-    }
+  this.createAppointment = function(userobj) {
+    return "sunday the 15th"
+  }
 
 };
-    
+  
 export const database = new WrapperDatabase();
 
 export default  { database }
