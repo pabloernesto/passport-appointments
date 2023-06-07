3bc6c054a47d1cb1d4020afc7acced45ad712400
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-06-07 11:02:10 -0300

    extracted formBody() into a module

--- app/request-handlers/appointment.js
+++ app/request-handlers/appointment.js
@@ -1,25 +1,9 @@
-import querystring from 'node:querystring';
 import { database } from './database-wrapper.js';
+import { formBody } from '../util-request.js';
+
 function match(req) {
   const { method, url } = req;
   return method === "POST" && url === "/appointment";
-}
-
-function formBody(request) {
-  return new Promise((resolve, reject) => {
-    let body = [];
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    }).on('end', () => {
-      body = Buffer.concat(body).toString();
-      // at this point, `body` has the entire request body stored in it as a string
-      body = querystring.parse(body);
-      console.log(body);
-      resolve(body)
-    }).on('error', (e) => {
-      reject(e);
-    });
-  });
 }
 
 function getAppointment(body) {
