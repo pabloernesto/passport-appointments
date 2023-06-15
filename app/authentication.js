d612397e80453a563271ca01907a33d73231ff56
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-06-15 01:09:02 -0300

    added dummy authentication

---
+++ app/authentication.js
@@ -0,0 +1,13 @@
+export function authenticateUser(username, password) {
+  return true;
+}
+
+export function generateSessionToken() {
+  return '1234';
+}
+
+export function isValidSessionToken(s) {
+  return true;
+}
+
+
