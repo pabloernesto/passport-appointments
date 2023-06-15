c4cbde4d9c46ca489de3e954644e3b7f93bd093e
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-06-15 20:08:44 -0300

    authentication.js: updated module interface
    refactored authentication into a class intead of a loose collection of
    functions. the exported class is instantiated with a database object.

--- app/authentication.js
+++ app/authentication.js
@@ -1,43 +1,56 @@
-import { randomBytes } from 'crypto';
-
-// key: token
-// value: { token: token, emitted: date }
-let validTokens = new Map();
+import { randomBytes, createHash } from 'crypto';
 
 const TOKEN_VALIDITY = 60 * 60 * 1000; // 1 hour
 const TOKEN_INVALIDATION_PERIOD = 5 * 60 * 1000; // 5 minutes
 
-export async function authenticateUser(username, password, database) {
-  const user = await database.getUser(username);
-  return user && user.hash === hash(password + user.salt);
+function hash(data) {
+  const hashFunction = createHash('sha256');
+  hashFunction.update(data);
+  return hashFunction.digest('base64url');
 }
 
-export function generateSessionToken() {
-  let sessionToken;
-  do {
-    const bytes = randomBytes(16);
-    sessionToken = bytes.toString('base64url');
-  } while (validTokens.has(sessionToken));
-  validTokens.set(sessionToken, {
-    token: sessionToken,
-    emitted: new Date(),
-  });
-  return sessionToken;
-}
+class Authentication {
+  constructor(database) {
+    this.database = database;
+    // key: token
+    // value: { token: token, emitted: date }
+    this.validTokens = new Map();
 
-export function isValidSessionToken(s) {
-  return validTokens.has(s);
-}
+    // Set up repeating timer to invalidate expired tokens
+    setInterval(() => this.invalidateExpiredTokens(), TOKEN_INVALIDATION_PERIOD);
+  }
 
-export function invalidateExpiredTokens() {
-  const expirationTime = new Date().getTime() - TOKEN_VALIDITY;
+  async authenticateUser(username, password) {
+    const user = await this.database.getUser(username);
+    return user && user.hash === hash(password + user.salt);
+  }
 
-  for (const { token } of validTokens.values()) {
-    if (tokenObject.emitted.getTime() <= expirationTime)
-      // modifying the map while iterating it doesn't seem to break the iterator
-      validTokens.delete(token);
+  generateSessionToken() {
+    let sessionToken;
+    do {
+      const bytes = randomBytes(16);
+      sessionToken = bytes.toString('base64url');
+    } while (this.validTokens.has(sessionToken));
+    this.validTokens.set(sessionToken, {
+      token: sessionToken,
+      emitted: new Date(),
+    });
+    return sessionToken;
+  }
+
+  isValidSessionToken(s) {
+    return this.validTokens.has(s);
+  }
+
+  invalidateExpiredTokens() {
+    const expirationTime = new Date().getTime() - TOKEN_VALIDITY;
+
+    for (const { token } of this.validTokens.values()) {
+      if (token.emitted.getTime() <= expirationTime)
+        // modifying the map while iterating it doesn't seem to break the iterator
+        validTokens.delete(token);
+    }
   }
 }
 
-// Set up repeating timer to invalidate expired tokens
-setInterval(invalidateExpiredTokens, TOKEN_INVALIDATION_PERIOD);
+export default Authentication;
