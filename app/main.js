2ea53bea357791fe7616dd884cc596b7e0e63ff9
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-07-23 17:52:40 -0300

    enabled static middleware

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 import Server from './lib/http/server.js';
 import http404MW from './lib/http/404.js';
 import AuthenticationMW from './lib/http/auth.js';
+import StaticFilesMW from './lib/http/static.js';
 import AppointmentsMW from './ui/http/appointment.js';
 // import AdminMW from './admin/http/admin.js';
 
@@ -13,6 +14,7 @@ let model = new Appointments(store);
 let server = new Server();
 server.add_middleware(new AuthenticationMW(store));
 server.add_middleware(new AppointmentsMW(model));
+server.add_middleware(await StaticFilesMW.fromPath("./app/assets"));
 // server.add_middleware(new AdminMW(model));
 server.add_middleware(new http404MW());
 server.listen('127.0.0.1', 3000);
