93f89194ab9be55fbd5cdadd0d07a79e157dab93
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-07-23 17:34:53 -0300

    main: enabled 404mw

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 import Server from './lib/http/server.js';
+import http404MW from './lib/http/404.js';
 import AuthenticationMW from './lib/http/auth.js';
 import AppointmentsMW from './ui/http/appointment.js';
 // import AdminMW from './admin/http/admin.js';
@@ -13,5 +14,5 @@ let server = new Server();
 server.add_middleware(new AuthenticationMW(store));
 server.add_middleware(new AppointmentsMW(model));
 // server.add_middleware(new AdminMW(model));
-// server.add_middleware(new http404MW());  // ?
+server.add_middleware(new http404MW());
 server.listen('127.0.0.1', 3000);
