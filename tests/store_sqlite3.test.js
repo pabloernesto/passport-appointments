81a993bc7a7c13ba775e664d88dc29dd0556f4b9
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-07-26 18:22:31 -0300

    updated store interface spec

--- tests/store_sqlite3.test.js
+++ tests/store_sqlite3.test.js
@@ -12,72 +12,66 @@ beforeEach(() => {
 
 
 /* Tests */
+test('creating a user', async () => {
+  await database.addUser("Superman", "superman@un.org", "ABCD", "EFGH");
+  let user = await database.getUser("Superman");
+
+  expect(user.user).toBe("Superman");
+  expect(user.email).toBe("superman@un.org");
+  expect(user.hash).toBe("ABCD");
+  expect(user.salt).toBe("EFGH");
+})
+
+// TODO: change this to fail if the appointment slot isn't available
+// TODO: change this to require an embassy site parameter
 test('create 3 users and 1 appointment', async () => {
-  await database.addUser({
-    user_id: "Superman",
-    email: "superman@un.org",
-    salt: "ABCD",
-    hash: "EFGH"
-  });
-
-  await database.addUser({
-  user_id: "Batman",
-  email: "batman@bat_base.org",
-  salt: "ABCD",
-  hash: "EFGH"
-  });
+  await database.addUser("Superman", "superman@un.org", "ABCD", "EFGH");
+  await database.addUser("Batman", "batman@bat_base.org", "ABCD", "EFGH");
+  await database.addUser("Wonder Woman", "wonderwoman@un.org", "ABCD", "EFGH");
   
-  await database.addUser({
-  user_id: "Wonder Woman",
-  email: "wonderwoman@un.org",
-  salt: "ABCD",
-  hash: "EFGH"
-  });
-
-  let user = await database.getUser("Wonder Woman");
-    
-  await database.createAppointment(user.user_id);
-  let appt = await database.fetchAppointment(user.user_id)
-  expect(appt.date).toBe("sunday the 1th");
+  let when = "2023-01-01";
+  let appt = await database.createAppointment("Wonder Woman", when);
+  
+  expect(appt.date).toBe(when);
 });
 
-test('create 3 appointments', async () => {
-  let pending_users = [];
-  pending_users +=  database.addUser({
-    user_id: "Superman",
-    email: "superman@un.org",
-    salt: "ABCD",
-    hash: "EFGH"
-  });
+// test('create 3 appointments', async () => {
+//   let pending_users = [];
+//   pending_users +=  database.addUser({
+//     user_id: "Superman",
+//     email: "superman@un.org",
+//     salt: "ABCD",
+//     hash: "EFGH"
+//   });
 
-  pending_users += database.addUser({
-  user_id: "Batman",
-  email: "batman@bat_base.org",
-  salt: "ABCD",
-  hash: "EFGH"
-  });
+//   pending_users += database.addUser({
+//   user_id: "Batman",
+//   email: "batman@bat_base.org",
+//   salt: "ABCD",
+//   hash: "EFGH"
+//   });
   
-  pending_users += database.addUser({
-  user_id: "Wonder Woman",
-  email: "wonderwoman@un.org",
-  salt: "ABCD",
-  hash: "EFGH"
-  });
+//   pending_users += database.addUser({
+//   user_id: "Wonder Woman",
+//   email: "wonderwoman@un.org",
+//   salt: "ABCD",
+//   hash: "EFGH"
+//   });
 
-  await Promise.all(pending_users);
+//   await Promise.all(pending_users);
   
-  let user = await database.getUser("Wonder Woman");
-  await database.createAppointment(user.user_id);
-  let appt = await database.fetchAppointment(user.user_id)
-  expect(appt.date).toBe("sunday the 1th");
+//   let user = await database.getUser("Wonder Woman");
+//   await database.createAppointment(user.user_id);
+//   let appt = await database.fetchAppointment(user.user_id)
+//   expect(appt.date).toBe("sunday the 1th");
 
-  user = await database.getUser("Batman");
-  await database.createAppointment(user.user_id);
-  appt = await database.fetchAppointment(user.user_id)
-  expect(appt.date).toBe("sunday the 2th");
+//   user = await database.getUser("Batman");
+//   await database.createAppointment(user.user_id);
+//   appt = await database.fetchAppointment(user.user_id)
+//   expect(appt.date).toBe("sunday the 2th");
 
-  user = await database.getUser("Superman");
-  await database.createAppointment(user.user_id);
-  appt = await database.fetchAppointment(user.user_id)
-  expect(appt.date).toBe("sunday the 3th");
-});
+//   user = await database.getUser("Superman");
+//   await database.createAppointment(user.user_id);
+//   appt = await database.fetchAppointment(user.user_id)
+//   expect(appt.date).toBe("sunday the 3th");
+// });
