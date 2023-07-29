9cb4629209b62fa9323d05bd71af7a2f33b8f7df
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-07-29 16:54:45 -0300

    model interface: user handling, store -> auth

--- tests/model_appointments.test.js
+++ tests/model_appointments.test.js
@@ -1,15 +1,18 @@
 import Appointments from "../app/model/appointments";
 import Store from '../app/storage/sqlite3/store';
+import Authentication from '../app/lib/auth'
 
 
 
 /* Test context */
 let model;
+let auth;
 let store;
 
 beforeEach(() => {
   store = Store.fromNewTestDB();
   model = new Appointments(store);
+  auth = new Authentication(store);
 })
 
 
@@ -21,6 +24,6 @@ test('given an empty store, model.getAppointment() throws with missing user', as
 })
 
 test('given a store with no appointments, model.getAppointment() returns undefined', async () => {
-  await store.addUser("Wonder Woman", "wonderwoman@un.org", "ABCD", "EFGH");
-  expect(model.getAppointment("Wonder Woman")).resolves.toBe(undefined);
+  await auth.createUser("Wonder Woman", "wonderwoman@un.org", "1234");
+  await expect(model.getAppointment("Wonder Woman")).resolves.toBe(undefined);
 })
