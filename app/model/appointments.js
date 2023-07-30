1313958775863e42c0838a1682757c1a2f2937db
Mafer <mf.pont.tv@gmail.com>
2023-07-30 13:48:07 -0300

    refactor error checks

--- app/model/appointments.js
+++ app/model/appointments.js
@@ -3,25 +3,27 @@ export default class Appointments {
     this._database = database;
   }
 
+  async findOpenAppointmentFor(user) {
+    
+  }
+
   /* appointments */
 
   // create
   async requestAppointment(user) {
     const has = await this._database.hasUser( { user_id: user} );
-    if(has) {
-      const appt = await this._database.hasAppointment(user);
-      if(!appt) {
-        await this._database.createAppointment(user, "1999-10-10 00:00:00");
+    if(!has) throw Error("No such user :/");
 
-        let db_object = await this._database.fetchAppointment(user);
-        return new String(db_object.date);
-      } else {
-        throw Error("Already has appointment");
-      }
-    } else {
-      throw Error("No such user :/");
-    }
-    
+    const appt = await this._database.hasAppointment(user);
+    if(appt) throw Error("Already has appointment");
+
+    await this._database.createAppointment(user, "1999-10-10 00:00:00");
+
+    let db_object = await this._database.fetchAppointment(user);
+    if (db_object && db_object.date) 
+      return new String(db_object.date);
+    else  
+      throw Error("Could not create appointment");
   }
 
   // read
