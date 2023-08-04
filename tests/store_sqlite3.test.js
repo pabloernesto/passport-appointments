1a35764eb57fb6555c2c64eb64d9feb2a514d3c4
Mafer <mf.pont.tv@gmail.com>
2023-08-04 00:01:07 -0300

    failing concurrency test

--- tests/store_sqlite3.test.js
+++ tests/store_sqlite3.test.js
@@ -145,15 +145,34 @@ test('given a queue with a user, when getting first user remove them from the qu
   .resolves.toBe(undefined);
 })
 
+test('given a queue, add 3 users and view 2 sequentially then one is left', async () => {
+  await fillWithSuperheroes(database);
+  await database.addUserToQueue("Superman");
+  await database.addUserToQueue("Batman");
+  await database.addUserToQueue("Wonder Woman2");
+  await expect(database.getFirstUserInQueue())
+  .resolves.toBe("Superman");
+  await expect(database.getFirstUserInQueue())
+  .resolves.toBe("Batman");
+  await expect(database.getFirstUserInQueue())
+  .resolves.toBe("Wonder Woman2");
+  await expect(database.getFirstUserInQueue())
+  .resolves.toBe(undefined);
+})
+
+
 test('given a queue, add 3 users and view 2 non sequentially then one is left', async () => {
   await fillWithSuperheroes(database);
-  await Promise.all([
+  
+  let results = await Promise.all([
     database.addUserToQueue("Superman"),
     database.addUserToQueue("Batman"),
     database.addUserToQueue("Wonder Woman2"),
     database.getFirstUserInQueue(),
     database.getFirstUserInQueue()
   ])
+  // getFirstUserInQueue should never return the same result twice
+  expect(results[3] != results[4]).toEqual(true);
   let leftover = await database.getFirstUserInQueue();
   await expect(["Superman", "Batman", "Wonder Woman2"].includes(leftover)).toEqual(true);
   await expect(database.getFirstUserInQueue()).resolves.toBe(undefined);
