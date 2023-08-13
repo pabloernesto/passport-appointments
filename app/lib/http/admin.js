638074295faeeb4d21cca225b39ba584fc2d942e
Mafer <mf.pont.tv@gmail.com>
2023-08-12 22:07:23 -0300

    admin works, dirty input is possible

--- app/lib/http/admin.js
+++ app/lib/http/admin.js
@@ -28,9 +28,42 @@ export default class AdminMW {
   async handleSlots(req, res) {
     // TODO: formBody not reading properly
     const form = await formBody(req);
-    const date_obj = fecha.parse(slot_date, "YYYY-MM-DDTHH:mm");
-    await this._model.createSlots([date_obj]);
+    const form_obj = slots_parse(form);
+    await this._model.createSlots([form_obj.range_start]);
   }
 }
 
+/*
+  validate form or fill with defualt values where needed
+*/
+export function slots_parse(form_data) {
+  //let object = {"weekday-mo": "on", "range-start":  "2001-01-01", "range-end": "2001-01-01"};
+  let properties = Object.getOwnPropertyNames(form_data);
+
+  // other than the checkbox data...
+  properties = properties.filter(p => !p.match('weekday')); 
+
+  // every property must have a value
+  let  valid = properties.every(p => form_data[p] &&  (form_data[p] != ''));
+  if(!valid) {
+    properties.map(p => {
+      form_data[p] = (form_data[p] == '') ? valid(p) : form_data[p];
+    })
+  }
+
+  // parse dates
+  const range_start = fecha.parse(form_data["range-start"], "YYYY-MM-DD");
+  const range_end = fecha.parse(form_data["range-end"], "YYYY-MM-DD");
+  const time_start = fecha.parse(form_data["time-start"], "HH:mm");
+  const time_end = fecha.parse(form_data["time-end"], "HH:mm");
+  const interval = parseInt(form_data["duration"]);
+
+  return {range_start, range_end, time_start, time_end, interval};  
+}
+
+function valid(property) {
+  if (["range-start", "range-end", "time-start", "time-end"].includes(property)) {
+    return new Date(Date.now());
+  }
+}
 
