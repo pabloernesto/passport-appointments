c3629527c45dc6a2e3655ce453e3bce8feeb1329
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2023-08-19 20:21:27 -0300

    admin_mw: formatting

--- tests/admin.test.js
+++ tests/admin.test.js
@@ -13,6 +13,12 @@ beforeEach(async () => {
 
 /* Tests */
 test('result range start is not undefined', async () => {
-  let result = slots_parse({"weekday-mo": "on", "range-start":  "2001-01-01", "range-end": "2001-01-01", "time-start":  "13:00", "time-end": "14:00"});
+  let result = slots_parse({
+    "weekday-mo": "on",
+    "range-start": "2001-01-01",
+    "range-end": "2001-01-01",
+    "time-start": "13:00",
+    "time-end": "14:00"
+  });
   expect(result.range_start).not.toEqual(undefined);
 })
